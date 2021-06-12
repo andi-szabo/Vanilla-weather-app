2bc70870e1b8de60295e4921f4ebd99d0853cc26
Andrea Szabo <szaboande93@gmail.com>
2021-06-12 18:20:22 +0200

    Removed console log

--- src/app.js
+++ src/app.js
@@ -77,14 +77,12 @@ function displayForecast(response) {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "3bca1d79b9964ec891a9f65830e74235";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
